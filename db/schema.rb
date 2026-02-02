68504176efec4b04bbe25bec48c5b5af292fa92d
safiya <bioelatrash@gmail.com>
2026-02-02 14:15:33 -0100

    add recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_02_150340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "author"
+    t.string "category"
+    t.integer "cook_time"
+    t.datetime "created_at", null: false
+    t.string "image"
+    t.text "ingredients", default: [], array: true
+    t.tsvector "ingredients_search_vector"
+    t.integer "prep_time"
+    t.decimal "ratings", precision: 3, scale: 2
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_recipes_on_category"
+    t.index ["ingredients"], name: "index_recipes_on_ingredients", using: :gin
+    t.index ["ingredients_search_vector"], name: "index_recipes_on_ingredients_search_vector", using: :gin
+    t.index ["ratings"], name: "index_recipes_on_ratings"
+    t.index ["title"], name: "index_recipes_on_title"
+  end
 end
